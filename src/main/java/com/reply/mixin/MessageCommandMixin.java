ffb6b0ec516f3602ee42c94a28be51a93c08f01e
BaconCat__ <126538523+BaconCat1@users.noreply.github.com>
2025-11-27 02:04:41 +0000

    refactor: update MessageCommandMixin to use CallbackInfoReturnable and thus not require signed messages.

--- src/main/java/com/reply/mixin/MessageCommandMixin.java
+++ src/main/java/com/reply/mixin/MessageCommandMixin.java
@@ -1,21 +1,20 @@
 package com.reply.mixin;
 
 import com.reply.Command;
-import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.command.MessageCommand;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collection;
 
 @Mixin(MessageCommand.class)
 public class MessageCommandMixin {
     @Inject(method = "execute", at = @At("HEAD"))
-    private static void onExecute(ServerCommandSource source, Collection<ServerPlayerEntity> targets, SignedMessage message, CallbackInfo ci) {
+    private static void onExecute(ServerCommandSource source, Collection<ServerPlayerEntity> targets, CallbackInfoReturnable<Integer> cir) {
         ServerPlayerEntity sender = source.getPlayer();
         if (sender != null) {
             for (ServerPlayerEntity target : targets)
